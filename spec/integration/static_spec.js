307a8f802657b22a0c74a92ff32e40fc88b3c85d
Nils Rohe <nilsrohe@freenet.de>
2019-07-02 19:06:17 +0200

    corrected the base in the spec file for testing

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -1,7 +1,6 @@
 const request = require("request");
 const server = require("../../src/server");
 const base = "http://localhost:3000/";
-const about = "http://localhost:3000/about";
 
 describe("routes : static", () => {
   describe("GET /", () => {
@@ -16,7 +15,7 @@ describe("routes : static", () => {
 
   describe("GET /about", () => {
     it("should return status code 200 and have 'About Us' in the body of the response", (done) => {
-      request.get(about, (err, res, body) => {
+      request.get(`${base}about`, (err, res, body) => {
         expect(res.statusCode).toBe(200);
         expect(body).toContain("About Us");
         done();
