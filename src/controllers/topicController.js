c81ee8e2b621159fbd0dde42ae3e492860ae8072
Nils Rohe <nilsrohe@freenet.de>
2019-07-03 19:42:04 +0200

    completed checkpoint - npm start not working

--- src/controllers/topicController.js
+++ src/controllers/topicController.js
@@ -1,5 +1,17 @@
+const topicQueries = require("../db/queries.topics.js");
+
 module.exports = {
-  index(req, res, next){
-    res.send("TODO: list all topics");
+  index(req, res, next) {
+    topicQueries.getAllTopics((err, topics) => {
+
+      //#3
+      if (err) {
+        res.redirect(500, "static/index");
+      } else {
+        res.render("topics/index", {
+          topics
+        });
+      }
+    });
   }
 };
