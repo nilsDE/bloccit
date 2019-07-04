89f40db7e253058635a079106119b7138fa233fe
Nils Rohe <nils.rohe@sosafe.de>
2019-07-04 19:31:10 +0200

    continue at edit and update

--- src/db/queries.topics.js
+++ src/db/queries.topics.js
@@ -1,17 +1,47 @@
 const Topic = require("./models").Topic;
 
 module.exports = {
-
-//#1
-  getAllTopics(callback){
+  getAllTopics(callback) {
     return Topic.all()
-
-//#2
-    .then((topics) => {
-      callback(null, topics);
-    })
-    .catch((err) => {
-      callback(err);
-    })
+      .then((topics) => {
+        callback(null, topics);
+      })
+      .catch((err) => {
+        callback(err);
+      });
+  },
+  getTopic(id, callback) {
+    return Topic.findById(id)
+      .then((topic) => {
+        callback(null, topic);
+      })
+      .catch((err) => {
+        callback(err);
+      });
+  },
+  addTopic(newTopic, callback) {
+    return Topic.create({
+        title: newTopic.title,
+        description: newTopic.description
+      })
+      .then((topic) => {
+        callback(null, topic);
+      })
+      .catch((err) => {
+        callback(err);
+      });
+  },
+  deleteTopic(id, callback) {
+    return Topic.destroy({
+        where: {
+          id
+        }
+      })
+      .then((topic) => {
+        callback(null, topic);
+      })
+      .catch((err) => {
+        callback(err);
+      });
   }
-}
+};
