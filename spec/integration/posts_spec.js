6043d5df8783db637d354b8b1c88828fd792ecbd
Nils Rohe <nils.rohe@sosafe.de>
2019-07-15 19:31:47 +0200

    completed checkpoint

--- spec/integration/posts_spec.js
+++ spec/integration/posts_spec.js
@@ -74,6 +74,32 @@ describe("routes : posts", () => {
         }
       );
     });
+    it("should not create a new post that fails validations", (done) => {
+      const options = {
+        url: `${base}/${this.topic.id}/posts/create`,
+        form: {
+          title: "a",
+          body: "b"
+        }
+      };
+      request.post(options,
+        (err, res, body) => {
+          Post.findOne({
+              where: {
+                title: "a"
+              }
+            })
+            .then((post) => {
+              expect(post).toBeNull();
+              done();
+            })
+            .catch((err) => {
+              console.log(err);
+              done();
+            });
+        }
+      );
+    });
   });
   describe("GET /topics/:topicId/posts/:id", () => {
     it("should render a view with the selected post", (done) => {
@@ -124,7 +150,8 @@ describe("routes : posts", () => {
       const options = {
         url: `${base}/${this.topic.id}/posts/${this.post.id}/update`,
         form: {
-          title: "Snowman Building Competition"
+          title: "Snowman Building Competition",
+          body: "I really enjoy the funny hats on them."
         }
       };
       request.post(options,
