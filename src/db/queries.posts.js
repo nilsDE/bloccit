60c7767ec77784649cccd78cdc0f328150d888ee
Nils Rohe <nils.rohe@sosafe.de>
2019-07-30 20:50:38 +0200

    completed checkpoint

--- src/db/queries.posts.js
+++ src/db/queries.posts.js
@@ -4,6 +4,7 @@ const Comment = require("./models").Comment;
 const User = require("./models").User;
 const Authorizer = require("../policies/post");
 const Vote = require("./models").Vote;
+const Favorite = require("./models").Favorite;
 
 module.exports = {
   addPost(newPost, callback) {
@@ -26,6 +27,9 @@ module.exports = {
         }, {
           model: Vote,
           as: "votes"
+        }, {
+          model: Favorite,
+          as: "favorites"
         }]
       })
       .then((post) => {
